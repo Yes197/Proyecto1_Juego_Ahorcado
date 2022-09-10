875eb9c3c595cd79dc2a10cc052385fb0c92fcef
Dan Joshua Santivañez Gutarra <83432705+djsg2021utec@users.noreply.github.com>
2022-09-10 13:08:36 -0500

    update2

---
+++ proyecto_ahorcado/src/proyecto_ahorcado/Jugador2.java
@@ -0,0 +1,35 @@
+package proyecto_ahorcado;
+
+import java.util.ArrayList;
+import antlr.collections.List;
+import jadex.bdiv3.annotation.Belief;
+import jadex.bdiv3.features.IBDIAgentFeature;
+import jadex.micro.annotation.Agent;
+import jadex.micro.annotation.AgentCreated;
+import jadex.micro.annotation.AgentFeature;
+import jadex.micro.annotation.Description;
+
+@Agent
+@Description("Jugador 1")
+public class Jugador2 {
+	@AgentFeature
+	protected IBDIAgentFeature bdifeature;
+	
+	@AgentCreated
+	
+	@Belief
+	public void PalabrasEspanhol()
+	{
+	  ArrayList<String> PalabrasEspanhol = new ArrayList<String>();
+	  PalabrasEspanhol.add("ceviche");
+	  PalabrasEspanhol.add("chanfainita");
+	  PalabrasEspanhol.add("pachamanca");
+	  PalabrasEspanhol.add("mondonguito");
+	  PalabrasEspanhol.add("mazamorra");
+	  PalabrasEspanhol.add("juane");
+	  PalabrasEspanhol.add("sancochado");
+	}
+	
+	
+	
+}
